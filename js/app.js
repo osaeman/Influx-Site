33abb4bc0ec6ed577f0e0c448bc703adb1f6c434
osaeman <2015n2424@gmail.com>
2024-01-12 01:03:46 +0500

    full website

--- js/app.js
+++ js/app.js
@@ -1,49 +1,8 @@
-// Header Appearance
-const header = document.querySelector("header");
+//  Mobile Header Appearance
+const hamburger = document.querySelector(".hamburger");
+const hamburger_menu = document.querySelector(".hamburger-menu");
 
-let lastScrollPosition = 0;
-
-window.addEventListener("scroll", () => {
-  const currentScrollPosition = window.scrollY;
-
-  // Check if the user is scrolling down
-  if (currentScrollPosition > lastScrollPosition) {
-    // Scrolling down, hide the header
-    header.style.transform = "translateY(-100%)";
-    header.style.backgroundColor = "transparent";
-  } else if (currentScrollPosition > 0) {
-    // Scrolling up, show the header
-    header.style.backgroundColor = "#655a3f";
-    header.style.transform = "translateY(0)";
-  } else {
-    header.style.backgroundColor = "";
-  }
-
-  lastScrollPosition = currentScrollPosition;
-});
-// Hamburger Window Display
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const hamburger_display = document.querySelector(".hamburger-display");
-  const close_btn = document.querySelector(".fa-times");
-  const header_btn = document.querySelector(".header-mob-button");
-
-  hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("is-active");
-    document.body.style.overflow = "hidden";
-    setTimeout(function () {
-      hamburger_display.style.display = "block";
-    }, 100);
-  });
-
-  close_btn.addEventListener("click", () => {
-    hamburger.classList.remove("is-active");
-    hamburger_display.style.display = "none";
-    document.body.style.overflow = "";
-  });
-  header_btn.addEventListener("click", () => {
-    hamburger.classList.remove("is-active");
-    hamburger_display.style.display = "none";
-    document.body.style.overflow = "";
-  });
+hamburger.addEventListener("click", () => {
+  hamburger_menu.classList.toggle("hamburger-menu-slide");
+  console.log("osama");
 });
